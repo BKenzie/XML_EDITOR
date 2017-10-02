741a3bacc289b7310d5d1a80f1eb0969129904b9
Braedyn Kenzie <braedyn.kenzie@stemcell.com>
2017-10-02 08:57:45 -0700

    Adding ActionPathXmlNode.cs class

---
+++ WPF_XML_Tutorial/ActionPathXmlNode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WPF_XML_Tutorial
+{
+    // Helper class for interchanging between different ActionPaths in the editor window
+    class ActionPathXmlNode
+    {
+        public int PathID
+        {
+            get; set;
+        }
+
+        public XmlNode XmlNode
+        {
+            get; set;
+        }
+
+        public ActionPathXmlNode( XmlNode xmlNode, int pathID )
+        {
+            PathID = pathID;
+            XmlNode = xmlNode;
+        }
+
+    }
+}
